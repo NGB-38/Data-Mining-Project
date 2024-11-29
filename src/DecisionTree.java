257dcecf35c9b9b8c57e06b130eb26d53ee7be06
Nguyen Gia Bao <153185278+NGB-38@users.noreply.github.com>
2024-11-29 11:38:21 +0700

    Evaluating J48 model

--- src/DecisionTree.java
+++ src/DecisionTree.java
@@ -1,5 +1,6 @@
 package src;
 
+import weka.classifiers.Evaluation;
 import weka.classifiers.trees.J48;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
@@ -33,5 +34,31 @@ public class DecisionTree {
         weka.core.SerializationHelper.write("Models/Decision_Tree.model", tree);
 
         J48 loadedTree = (J48) weka.core.SerializationHelper.read("Models/Decision_Tree.model");
+
+        //Evaluation
+        long startTime = System.currentTimeMillis(); // Record start time
+        Evaluation eval = new Evaluation(newData);
+        eval.crossValidateModel(loadedTree, newData, 10, new java.util.Random(1));
+        long endTime = System.currentTimeMillis();
+
+        // Calculate runtime
+        long runtimeMillis = endTime - startTime;
+        double runtimeSeconds = runtimeMillis / 1000.0;
+
+        System.out.println(eval.toSummaryString("Evaluation results:\n", false));
+        System.out.println("Correct % = "+eval.pctCorrect());
+        System.out.println("Incorrect % = "+eval.pctIncorrect());
+        System.out.println("AUC = " + eval.areaUnderROC(0));
+        System.out.println("kappa = " + eval.kappa());
+        System.out.println("MAE = " + eval.meanAbsoluteError());
+        System.out.println("RMSE = " + eval.rootMeanSquaredError());
+        System.out.println("RAE = " + eval.relativeAbsoluteError());
+        System.out.println("RRSE = " + eval.rootRelativeSquaredError());
+        System.out.println("Precison = " + eval.precision(0));
+        System.out.println("Recall = " + eval.recall(0));
+        System.out.println("fMeasure = " + eval.fMeasure(0));
+        System.out.println("Error Rate = " + eval.errorRate());
+        System.out.println(eval.toMatrixString("\n=== Overall Confusion Matrix ===\n"));
+        System.out.println("Runtime (seconds): " + runtimeSeconds);
     }
 }
