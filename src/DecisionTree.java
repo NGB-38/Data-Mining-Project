d8cd681ec8287b466727368656003cf6c5ffa3e7
Nguyen Gia Bao <153185278+NGB-38@users.noreply.github.com>
2024-12-03 08:39:48 +0700

    Visualize decision tree

--- src/DecisionTree.java
+++ src/DecisionTree.java
@@ -6,17 +6,23 @@ import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
 import java.io.File;
 import java.util.Random;
+import weka.gui.treevisualizer.PlaceNode2;
+import weka.gui.treevisualizer.TreeVisualizer;
+import javax.swing.*;
 
 public class DecisionTree {
     public static void main(String[] args) throws Exception {
+        //Loading data
         DataSource source = new DataSource("Dataset/TitanicNom.arff");
         Instances dataset = source.getDataSet();
 
+        //Setting target feature
         dataset.setClassIndex(0);
 
+        //Building model
         J48 tree = new J48();
         tree.buildClassifier(dataset);
-        System.out.println(tree.graph());
+//        System.out.println(tree.graph());
 
         weka.core.SerializationHelper.write("Models/Decision_Tree.model", tree);
 
@@ -33,16 +39,16 @@ public class DecisionTree {
         double runtimeSeconds = runtimeMillis / 1000.0;
 
         System.out.println(eval.toSummaryString("Evaluation results:\n", false));
-//        System.out.println("AUC = " + eval.areaUnderROC(0));
-//        System.out.println("kappa = " + eval.kappa());
-//        System.out.println("MAE = " + eval.meanAbsoluteError());
-//        System.out.println("RMSE = " + eval.rootMeanSquaredError());
-//        System.out.println("RAE = " + eval.relativeAbsoluteError());
-//        System.out.println("RRSE = " + eval.rootRelativeSquaredError());
-//        System.out.println("Precison = " + eval.precision(0));
-//        System.out.println("Recall = " + eval.recall(0));
-//        System.out.println("fMeasure = " + eval.fMeasure(0));
-//        System.out.println("Error Rate = " + eval.errorRate());
+        System.out.println("AUC = " + eval.areaUnderROC(0));
+        System.out.println("kappa = " + eval.kappa());
+        System.out.println("MAE = " + eval.meanAbsoluteError());
+        System.out.println("RMSE = " + eval.rootMeanSquaredError());
+        System.out.println("RAE = " + eval.relativeAbsoluteError());
+        System.out.println("RRSE = " + eval.rootRelativeSquaredError());
+        System.out.println("Precison = " + eval.precision(0));
+        System.out.println("Recall = " + eval.recall(0));
+        System.out.println("fMeasure = " + eval.fMeasure(0));
+        System.out.println("Error Rate = " + eval.errorRate());
         System.out.println("Precision (Not survived) = " + eval.precision(0) + ", Precision (Survived) = " + eval.precision(1));
         System.out.println("Recall (Not survived) = " + eval.recall(0) + ", Recall (Survived) = " + eval.recall(1));
         System.out.println("F-Measure (Not survived) = " + eval.fMeasure(0) + ", F-Measure (Survived) = " + eval.fMeasure(1));
@@ -50,7 +56,13 @@ public class DecisionTree {
         System.out.println(eval.toMatrixString("\n=== Overall Confusion Matrix ===\n"));
         System.out.println("Runtime (seconds): " + runtimeSeconds);
 
-        // Evalutation using testing dataset
+        // Visualize the tree in a GUI
+        TreeVisualizer tv = new TreeVisualizer(null, tree.graph(), new PlaceNode2());
 
+        JFrame frame = new JFrame("J48 Decision Tree");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+        frame.getContentPane().add(tv);
+        frame.setVisible(true);
     }
 }
